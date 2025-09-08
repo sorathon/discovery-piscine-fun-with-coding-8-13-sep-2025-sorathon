af71e9e2a35e9320c597fe72f73bc06847f8c07e
sorathon <120354929+sorathon@users.noreply.github.com>
2025-09-08 21:45:11 +0700

    up

--- cell00/argv.sh
+++ cell00/argv.sh
@@ -3,8 +3,8 @@
 if [ -z "$1" ]; then
     echo "no argument"
 else
-    # แสดง argument สูงสุด 3 ตัว
-    echo "Argument 1: $1"
-    echo "Argument 2: $2"
-    echo "Argument 3: $3"
+    
+    echo "$1"
+    echo "$2"
+    echo "$3"
 fi
